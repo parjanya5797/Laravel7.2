f94a775fac21b47603d6bd345cd5aa79de5a8095
parjanya5797 <39951698+parjanya5797@users.noreply.github.com>
2019-02-22 09:56:13 +0545

    Laravel Controller route

---
+++ categories/create.blade.php
@@ -0,0 +1,42 @@
+@extends('layout')
+
+@section('content')
+<style>
+  .uper {
+    margin-top: 40px;
+  }
+</style>
+<div class="card uper">
+  <div class="card-header">
+    Add Category
+  </div>
+  <div class="card-body">
+    @if ($errors->any())
+      <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+              <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+      </div><br />
+    @endif
+      <form method="post" action="{{ route('categorys.store') }}">
+          <div class="form-group">
+              @csrf
+              <label for="name">Category Name:</label>
+              <input type="text" class="form-control" name="name"/>
+          </div>
+          <div class="form-group">
+              <label for="price">Category Description :</label>
+              <input type="text" class="form-control" name="description"/>
+          </div>
+          <div class="form-group">
+              <label for="quantity">Status:</label>
+             <input type="radio" name="status" value="1">1</br>
+             <input type="radio" name="status" value="0">0</br>
+          </div>
+          <button type="submit" class="btn btn-primary">Add</button>
+      </form>
+  </div>
+</div>
+@endsection
